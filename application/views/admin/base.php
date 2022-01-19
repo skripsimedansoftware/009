9cf9cbba5378c9a6708c1733ab4ddbce9c622b23
agoenks29D <agungmasda29@gmail.com>
2022-01-20 04:29:08 +0700

    update views for first time

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -77,93 +77,6 @@ desired effect
 			<!-- Navbar Right Menu -->
 			<div class="navbar-custom-menu">
 				<ul class="nav navbar-nav">
-					<!-- Messages: style can be found in dropdown.less-->
-					<li class="dropdown messages-menu">
-						<!-- Menu toggle button -->
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-envelope-o"></i>
-							<span class="label label-success">4</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 4 messages</li>
-							<li>
-								<!-- inner menu: contains the messages -->
-								<ul class="menu">
-									<li><!-- start message -->
-										<a href="#">
-											<div class="pull-left">
-												<!-- User Image -->
-												<img src="<?= (!empty($user->photo))?base_url('uploads/'.$user->photo):base_url('assets/adminlte/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
-											</div>
-											<!-- Message title and timestamp -->
-											<h4>Support Team<small><i class="fa fa-clock-o"></i> 5 mins</small></h4>
-											<!-- The message -->
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<!-- end message -->
-								</ul>
-								<!-- /.menu -->
-							</li>
-							<li class="footer"><a href="#">See All Messages</a></li>
-						</ul>
-					</li>
-					<!-- /.messages-menu -->
-
-					<!-- Notifications Menu -->
-					<li class="dropdown notifications-menu">
-						<!-- Menu toggle button -->
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-bell-o"></i>
-							<span class="label label-warning">10</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 10 notifications</li>
-							<li>
-								<!-- Inner Menu: contains the notifications -->
-								<ul class="menu">
-									<li><!-- start notification -->
-										<a href="#"><i class="fa fa-users text-aqua"></i> 5 new members joined today</a>
-									</li>
-									<!-- end notification -->
-								</ul>
-							</li>
-							<li class="footer"><a href="#">View all</a></li>
-						</ul>
-					</li>
-					<!-- Tasks Menu -->
-					<li class="dropdown tasks-menu">
-						<!-- Menu Toggle Button -->
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-flag-o"></i>
-							<span class="label label-danger">9</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 9 tasks</li>
-							<li>
-								<!-- Inner menu: contains the tasks -->
-								<ul class="menu">
-									<li><!-- Task item -->
-										<a href="#">
-											<!-- Task title and progress text -->
-											<h3>Design some buttons <small class="pull-right">20%</small></h3>
-											<!-- The progress bar -->
-											<div class="progress xs">
-												<!-- Change the css width attribute to simulate progress -->
-												<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-													<span class="sr-only">20% Complete</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<!-- end task item -->
-								</ul>
-							</li>
-							<li class="footer">
-								<a href="#">View all tasks</a>
-							</li>
-						</ul>
-					</li>
 					<!-- User Account Menu -->
 					<li class="dropdown user user-menu">
 						<!-- Menu Toggle Button -->
@@ -178,24 +91,9 @@ desired effect
 							<li class="user-header">
 								<img src="<?= (!empty($user->photo))?base_url('uploads/'.$user->photo):base_url('assets/adminlte/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
 								<p>
-									<?= $user->full_name ?> - Web Developer
-									<small>Member since Nov. 2012</small>
+									<?= $user->full_name ?>
+									<small>Skripsi</small>
 								</p>
-							</li>
-							<!-- Menu Body -->
-							<li class="user-body">
-								<div class="row">
-									<div class="col-xs-4 text-center">
-										<a href="#">Followers</a>
-									</div>
-									<div class="col-xs-4 text-center">
-										<a href="#">Sales</a>
-									</div>
-									<div class="col-xs-4 text-center">
-										<a href="#">Friends</a>
-									</div>
-								</div>
-								<!-- /.row -->
 							</li>
 							<!-- Menu Footer-->
 							<li class="user-footer">
@@ -208,8 +106,6 @@ desired effect
 							</li>
 						</ul>
 					</li>
-					<!-- Control Sidebar Toggle Button -->
-					<li><a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a></li>
 				</ul>
 			</div>
 		</nav>
@@ -245,17 +141,16 @@ desired effect
 
 			<!-- Sidebar Menu -->
 			<ul class="sidebar-menu" data-widget="tree">
-				<li class="header">HEADER</li>
+				<li class="header">MENU</li>
 				<!-- Optionally, you can add icons to the links -->
-				<li class="<?= $this->router->fetch_method() == 'index'?'active':'' ?>"><a href="<?= base_url($this->router->fetch_class()) ?>"><i class="fa fa-home"></i> <span>Home</span></a></li>
-				<li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+				<li class="<?= $this->router->fetch_method() == 'index'?'active':'' ?>"><a href="<?= base_url($this->router->fetch_class()) ?>"><i class="fa fa-home"></i> <span>Beranda</span></a></li>
 				<li class="treeview">
-					<a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+					<a href="#"><i class="fa fa-cubes"></i> <span>Data</span>
 						<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 					</a>
 					<ul class="treeview-menu">
-						<li><a href="#">Link in level 2</a></li>
-						<li><a href="#">Link in level 2</a></li>
+						<li><a href="#">Produk</a></li>
+						<li><a href="#">Pembelian</a></li>
 					</ul>
 				</li>
 			</ul>
@@ -274,10 +169,10 @@ desired effect
 	<footer class="main-footer">
 		<!-- To the right -->
 		<div class="pull-right hidden-xs">
-			Anything you want
+			SKRIPSI
 		</div>
 		<!-- Default to the left -->
-		<strong>Copyright &copy; <?= date('Y') ?> <a href="#">Medan Software</a>.</strong> All rights reserved.
+		<strong>Copyright &copy; <?= date('Y') ?> <a href="#"><?= $this->config->item('app_name'); ?></a>.</strong> All rights reserved.
 	</footer>
 
 	<!-- Control Sidebar -->
